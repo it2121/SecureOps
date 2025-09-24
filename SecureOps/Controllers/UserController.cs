ccd883457e7d674f46b191a3db334a11b858478a
it2121 <phpmoon21@gmail..com>
2025-09-24 19:11:29 +0300

    users tbl

---
+++ SecureOps/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureOps.Data;
+using SecureOps.Models.Dto;
+
+
+namespace SecureOps.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+    private readonly AppDbContext _db;
+        // Temporary in-memory users (replace with DB later)
+
+
+        // GET: api/users
+        public UsersController(AppDbContext db)
+        {
+            _db = db;
+          
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _db.Users
+                .Select(u => new UserDto
+                {
+                    Email = u.Email,
+                    FullName = u.FullName,
+                    Username = u.Username
+                })
+                .ToListAsync();
+
+            return Ok(users);
+        }
+
+
+
+    }
+
+  
+ 
+}
---
+++ SecureOps/Models/Dto/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SecureOps.Models.Dto
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+
+    }
+}
